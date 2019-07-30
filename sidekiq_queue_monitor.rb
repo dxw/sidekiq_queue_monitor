81dfd814d017ba9488dbd9b00f5e4e2c60d505fb
Stuart Harrison <pezholio@gmail.com>
2019-07-30 13:44:56 +0100

    Wrap in a handler method
    Because that's what cloudwatch expects

--- sidekiq_queue_monitor.rb
+++ sidekiq_queue_monitor.rb
@@ -3,31 +3,32 @@ require 'date'
 
 # Set cloudwatch config
 
-cloudwatch = Aws::CloudWatch::Client.new(
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['SECRET_ACCESS_KEY']
-)
+def handler(event:, context:)
+  cloudwatch = Aws::CloudWatch::Client.new(
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['SECRET_ACCESS_KEY']
+  )
 
-# Get enqueued jobs from Redis
-stats = Sidekiq::Stats.new
-enqueued = stats.enqueued
+  # Get enqueued jobs from Redis
+  stats = Sidekiq::Stats.new
+  enqueued = stats.enqueued
 
-# Send value of `enqueued` to Cloudwatch
-cloudwatch.put_metric_data(
-  namespace: 'Redis',
-  metric_data: [
-    {
-      metric_name: 'Enqueued Jobs',
-      dimensions: [
-        {
-          name: "Site",
-          value: "StatsUserNet",
-        },
-      ],
-      timestamp: DateTime.now,
-      value: enqueued,
-      unit: 'Count'
-    }
-  ]
-)
-
+  # Send value of `enqueued` to Cloudwatch
+  cloudwatch.put_metric_data(
+    namespace: 'Redis',
+    metric_data: [
+      {
+        metric_name: 'Enqueued Jobs',
+        dimensions: [
+          {
+            name: "Site",
+            value: "StatsUserNet",
+          },
+        ],
+        timestamp: DateTime.now,
+        value: enqueued,
+        unit: 'Count'
+      }
+    ]
+  )
+end
